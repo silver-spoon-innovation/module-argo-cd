fe7feddbf6c54eba7500fd73c611b79e585e8b4e
pamornpolh <pamornpol.hanmatheekuna@gmail.com>
2022-12-27 15:10:11 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "kubernetes" {
+	cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+	host                   = var.kubernetes_cluster_endpoint
+	exec {
+		api_version = "client.authentication.k8s.io/v1beta1"
+		command     = "aws"
+		args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+	}
+}
+
+provider "helm" {
+	kubernetes {
+		cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+		host                   = var.kubernetes_cluster_endpoint
+		exec {
+			api_version = "client.authentication.k8s.io/v1beta1"
+			command     = "aws"
+			args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+		}
+	}
+}
+
+resource "kubernetes_namespace" "ns_argocd" {
+  metadata {
+    annotations = {
+      name = "argocd"
+    }
+
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = kubernetes_namespace.ns_argocd.metadata.0.name
+  create_namespace = false
+
+  depends_on = [kubernetes_namespace.ns_argocd]
+}
